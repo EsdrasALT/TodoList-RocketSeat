b51752f979868d416334f29490c40f86a34b248c
EsdrasALT <esdras.a.pessoal@gmail.com>
2024-12-04 10:08:45 -0300

    update

--- src/main/java/br/com/fernandoafonso/todolist/filter/FIlterTaskAuth.java
+++ src/main/java/br/com/fernandoafonso/todolist/filter/FIlterTaskAuth.java
@@ -1,58 +1,58 @@
-package br.com.fernandoafonso.todolist.filter;
+// package br.com.fernandoafonso.todolist.filter;
 
-import java.io.IOException;
-import java.util.Base64;
+// import java.io.IOException;
+// import java.util.Base64;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.stereotype.Component;
+// import org.springframework.web.filter.OncePerRequestFilter;
 
-import at.favre.lib.crypto.bcrypt.BCrypt;
-import br.com.fernandoafonso.todolist.user.IUserRepository;
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+// import at.favre.lib.crypto.bcrypt.BCrypt;
+// import br.com.fernandoafonso.todolist.user.IUserRepository;
+// import jakarta.servlet.FilterChain;
+// import jakarta.servlet.ServletException;
+// import jakarta.servlet.http.HttpServletRequest;
+// import jakarta.servlet.http.HttpServletResponse;
 
-@Component
-public class FIlterTaskAuth extends OncePerRequestFilter{
+// @Component
+// public class FIlterTaskAuth extends OncePerRequestFilter{
 
-    @Autowired
-    private IUserRepository userRepository;
+//     @Autowired
+//     private IUserRepository userRepository;
 
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-            throws ServletException, IOException {
+//     @Override
+//     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+//             throws ServletException, IOException {
 
-                var servletPath = request.getServletPath();
-                if(servletPath.startsWith("/tasks/")){
-                    var authorization = request.getHeader("Authorization");
+//                 var servletPath = request.getServletPath();
+//                 if(servletPath.startsWith("/tasks/")){
+//                     var authorization = request.getHeader("Authorization");
 
-                    var authEncode = authorization.substring("Basic".length()).trim();
+//                     var authEncode = authorization.substring("Basic".length()).trim();
 
-                    byte[] authDecode = Base64.getDecoder().decode(authEncode);
+//                     byte[] authDecode = Base64.getDecoder().decode(authEncode);
                     
-                    var authString = new String(authDecode);
+//                     var authString = new String(authDecode);
 
-                    String[] credentials = authString.split(":");
-                    String username = credentials[0];
-                    String password = credentials[1];
+//                     String[] credentials = authString.split(":");
+//                     String username = credentials[0];
+//                     String password = credentials[1];
 
-                    var user = this.userRepository.findByUsername(username);
-                    if(user == null){
-                        response.sendError(401);
-                    }else{
-                        var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
-                        if(passwordVerify.verified){
-                            request.setAttribute("idUser", user.getId());
-                            filterChain.doFilter(request, response);
-                            System.out.println("ok");
-                        }else{
-                            response.sendError(401);
-                        }
-                    }
-                }else{
-                    filterChain.doFilter(request, response);
-                }
-    }
-}
+//                     var user = this.userRepository.findByUsername(username);
+//                     if(user == null){
+//                         response.sendError(401);
+//                     }else{
+//                         var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+//                         if(passwordVerify.verified){
+//                             request.setAttribute("idUser", user.getId());
+//                             filterChain.doFilter(request, response);
+//                             System.out.println("ok");
+//                         }else{
+//                             response.sendError(401);
+//                         }
+//                     }
+//                 }else{
+//                     filterChain.doFilter(request, response);
+//                 }
+//     }
+// }
